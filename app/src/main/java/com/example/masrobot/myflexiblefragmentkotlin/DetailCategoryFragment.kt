df753f220c03378cce94a4e85216598c992a3b16
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2018-03-19 22:54:26 +0700

    DialogFragment dan Activity Profile

--- app/src/main/java/com/example/masrobot/myflexiblefragmentkotlin/DetailCategoryFragment.kt
+++ app/src/main/java/com/example/masrobot/myflexiblefragmentkotlin/DetailCategoryFragment.kt
@@ -1,8 +1,10 @@
 package com.example.masrobot.myflexiblefragmentkotlin
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -42,10 +44,19 @@ class DetailCategoryFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_profile -> {
+                val mIntent = Intent(activity, ProfileActivity::class.java)
+                startActivity(mIntent)
             }
             R.id.btn_show_dialog -> {
                 val mOptionDialogFragment = OptionDialogFragment()
-                mOptionDialogFragment.onOptionDialogListener
+                mOptionDialogFragment.onOptionDialogListener = object : OptionDialogFragment.OnOptionDialogListener {
+                    override fun onOptionChoosen(text: String?) {
+                        Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
+                    }
+                }
+                val mFragmentManager: FragmentManager = childFragmentManager
+                mOptionDialogFragment.show(mFragmentManager,
+                        OptionDialogFragment::class.java.simpleName)
             }
         }
     }
---
+++ app/src/main/java/com/example/masrobot/myflexiblefragmentkotlin/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.masrobot.myflexiblefragmentkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
